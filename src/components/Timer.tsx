d5920523621e433d4e147d30182ec14f02d4b7b0
makcsholonik <makcsholonik@gmail.com>
2022-02-17 15:24:54 +0300

    SettingsButton

--- src/components/Timer.tsx
+++ src/components/Timer.tsx
@@ -3,6 +3,7 @@ import { buildStyles, CircularProgressbar } from 'react-circular-progressbar';
 import 'react-circular-progressbar/dist/styles.css';
 import { PlayButton } from './PlayButton';
 import { PauseButton } from './PauseButton';
+import { SettingsButton } from './SettingsButton';
 
 const red = '#f54e4e';
 const green = '#4aec8c';
@@ -21,10 +22,12 @@ export const Timer : FC = () => {
 					trailColor : 'rgba(255,255,255,0.2)'
 				} ) }
 			/>
-			{/*@ts-ignore*/ }
 			<div style={ { marginTop : '20px' } }>
 				<PlayButton/>
 				<PauseButton/>
+			</div>
+			<div>
+				<SettingsButton/>
 			</div>
 		</div>
 	);
